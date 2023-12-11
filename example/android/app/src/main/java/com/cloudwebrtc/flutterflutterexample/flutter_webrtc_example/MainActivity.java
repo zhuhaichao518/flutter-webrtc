61fa89ecdcf289bba724b3eee7120f2fe70bd586
zhuhaichao518 <zhuhaichao518@gmail.com>
2023-12-12 04:49:49 +0800

    remove examples

--- example/android/app/src/main/java/com/cloudwebrtc/flutterflutterexample/flutter_webrtc_example/MainActivity.java
+++
@@ -1,6 +0,0 @@
-package com.cloudwebrtc.flutterflutterexample.flutter_webrtc_example;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-}
